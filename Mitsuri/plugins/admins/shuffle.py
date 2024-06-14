e2ae1e5f3e1c644ed5ca7a5716f63ca6e65602cb
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-06-14 16:16:44 +0530

    Update shuffle.py

--- Mitsuri/plugins/admins/shuffle.py
+++ Mitsuri/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from Mitsuri import app
+from Mitsuri.misc import db
+from Mitsuri.utils.decorators import AdminRightsCheck
+from Mitsuri.utils.inline import close_markup
 from config import BANNED_USERS
 
 
