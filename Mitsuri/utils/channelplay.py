9c547d5bd1da4f7f297c11e961619abada757c06
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-16 18:01:21 +0530

    Update channelplay.py

--- Mitsuri/utils/channelplay.py
+++ Mitsuri/utils/channelplay.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_cmode
+from Mitsuri import app
+from Mitsuri.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
