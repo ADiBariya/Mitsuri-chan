bb5fb538c81fa3107445ea9fd2829f6648683336
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-17 17:17:29 +0530

    Update queue.py

--- Mitsuri/utils/inline/queue.py
+++ Mitsuri/utils/inline/queue.py
@@ -1,6 +1,6 @@
 from typing import Union
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from Mitsuri import app
+from Mitsuri.utils.formatters import time_to_seconds
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
