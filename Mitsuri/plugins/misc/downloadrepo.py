f865ae42a96b3805943cc1cd1011c24f90681e82
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-17 20:50:44 +0530

    Update downloadrepo.py

--- Mitsuri/plugins/misc/downloadrepo.py
+++ Mitsuri/plugins/misc/downloadrepo.py
@@ -1,7 +1,7 @@
 """**
 MIT License
 
-Copyright (c) [All] [DAXX TEAM]
+Copyright (c) [All] [DEVS OF MITSURI]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ from pyrogram import Client, filters
 import git
 import shutil
 import os
-from DAXXMUSIC import app
+from Mitsuri import app
 
 
 
