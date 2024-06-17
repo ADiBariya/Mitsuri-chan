a2651a02aebc072b2e75b1dc10f0b749a8cab4a5
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-17 20:49:43 +0530

    Update broadcast.py

--- Mitsuri/plugins/misc/broadcast.py
+++ Mitsuri/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from Mitsuri import app
+from Mitsuri.misc import SUDOERS
+from Mitsuri.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.formatters import alpha_to_int
+from Mitsuri.utils.decorators.language import language
+from Mitsuri.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
