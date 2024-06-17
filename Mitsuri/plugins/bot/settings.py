c3b1e95f5fdf570725f37dd599cf770168fd7745
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-17 19:19:24 +0530

    Update settings.py

--- Mitsuri/plugins/bot/settings.py
+++ Mitsuri/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import (
+from Mitsuri import app
+from Mitsuri.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from DAXXMUSIC.utils.database import (
     skip_off,
     skip_on,
 )
-from DAXXMUSIC.utils.decorators.admins import ActualAdminCB
-from DAXXMUSIC.utils.decorators.language import language, languageCB
-from DAXXMUSIC.utils.inline.settings import (
+from Mitsuri.utils.decorators.admins import ActualAdminCB
+from Mitsuri.utils.decorators.language import language, languageCB
+from Mitsuri.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from DAXXMUSIC.utils.inline.start import private_panel
+from Mitsuri.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
