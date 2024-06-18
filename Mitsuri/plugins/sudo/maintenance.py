c3afeb433ad2cd14a59460959f794d7fd5cce59e
Obanai <157609132+mikeyy23254@users.noreply.github.com>
2024-06-18 23:08:17 +0530

    Update maintenance.py

--- Mitsuri/plugins/sudo/maintenance.py
+++ Mitsuri/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from Mitsuri import app
+from Mitsuri.misc import SUDOERS
+from Mitsuri.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
