054ef679299504f46d79cbce14abed204ff1c6fe
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-06-20 01:27:31 +0530

    Update unbanall.py

--- Mitsuri/plugins/admins/unbanall.py
+++ Mitsuri/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from Mitsuri import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from Mitsuri.utils.Mitsuri_ban import admin_filter
+from Mitsuri.utils.mitsuri_ban import admin_filter
 
 BOT_ID = "6561748036"
 
