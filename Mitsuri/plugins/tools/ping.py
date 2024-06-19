5ca428d692af90460f0e78790cbc85908fbc02c3
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-06-20 01:40:53 +0530

    Update ping.py

--- Mitsuri/plugins/tools/ping.py
+++ Mitsuri/plugins/tools/ping.py
@@ -4,23 +4,23 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from config import *
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils import bot_sys_stats
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.inline import supp_markup
+from Mitsuri import app
+from Mitsuri.core.call import Mitsuri 
+from Mitsuri.utils import bot_sys_stats
+from Mitsuri.utils.decorators.language import language
+from Mitsuri.utils.inline import supp_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "Mitsuri "]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://te.legra.ph/file/9415510370c4e307b100f.mp4",
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await DAXX.ping()
+    pytgping = await Mitsuri.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
