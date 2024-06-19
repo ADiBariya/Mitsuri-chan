bd3466cf32b100e072b5577af459acf8f133627c
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-06-20 01:55:54 +0530

    Update Gpt.py

--- Mitsuri/plugins/tools/Gpt.py
+++ Mitsuri/plugins/tools/Gpt.py
@@ -1,13 +1,13 @@
 import random
 import time
 import requests
-from DAXXMUSIC import app
+from Mitsuri import app
 from config import BOT_USERNAME
 
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","Mitsuri "]))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
