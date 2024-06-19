11d63865082baffc2b14df453f32d77fcb3ada58
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-06-20 01:25:32 +0530

    Update speed.py

--- Mitsuri/plugins/admins/speed.py
+++ Mitsuri/plugins/admins/speed.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from Mitsuri import app
-from Mitsuri.core.call import DAXX
+from Mitsuri.core.call import Mitsuri 
 from Mitsuri.misc import SUDOERS, db
 from Mitsuri.utils import AdminRightsCheck
 from Mitsuri.utils.database import is_active_chat, is_nonadmin_chat
@@ -94,7 +94,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await DAXX.speedup_stream(
+        await Mitsuri.speedup_stream(
             chat_id,
             file_path,
             speed,
